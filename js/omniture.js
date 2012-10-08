2f20068648565d72382f6cfde34d46def2104342
chase.martin@gmail.com <chase.martin@gmail.com>
2012-10-08 10:21:26 -0700

    finalized version including omniture.js wrapper code

---
+++ js/omniture.js
@@ -0,0 +1,74 @@
+//Requires bootstrap.js, coherent.js, apple.js and metrics.js
+//Compile to omniture_compressed.js with all dependencies prepended
+
+/**
+ * Singleton wrapper for Apple Metrics
+ * Currently supports fire and click omniture events
+ */
+var Omniture = function () {
+    "use strict";
+	
+	// Saved config parameter to be passed to init from batch()
+	var _batch = null;
+	
+	/**
+	 * Wrap all omniture calls here
+	 * @param callback: {function} the omniture call to be made
+	 */
+	function omniture (callback) {
+	    // Make sure apple metrics is available
+	    if (typeof (window.s) === "object") callback.call();
+	};
+    
+    // public
+	return {
+	    /**
+    	 * Initialize multiple omniture calls, see fire and click methods
+    	 * @param config: {json} { fire/click: [ parameters ]} 
+    	 */
+	    init: function (config){
+            for(var fun in config) {
+	            // Call the fire or click method and pass in an array of parameters
+	            Omniture[fun].apply(this, config[fun]);
+	        }
+	    },
+	    
+	    /**
+	     * Wrapper function for init, allows delayed execution of Omniture.fire events
+	     * Use Omniture.batch() with no arguments to call the saved batch.
+     	 * Note: Omnicure.click events cannot be delayed easily (without a custom loadEvent etc)
+     	 * @param delay: {bool} if false, batch is not saved and init will be called immediately
+	     */
+	    batch: function (config, delay) {
+	        if (delay === false)
+	            Omniture.init(config);
+	        else if (config === undefined)
+	            Omniture.init(_batch);
+	        else
+	            _batch = config;
+	    },
+	    
+	    /**
+    	 * Call apple metrics fireEvent method
+    	 * @param vars: {json} parameters required by apple metrics 
+    	 */
+	    fire: function (vars) {
+	        omniture(function(){ apple.metrics.fireEvent(vars); });
+	    },
+	    
+	    /**
+    	 * Setup a click event to call apple metrics fireEvent onclick
+    	 * @param selector: {string} selector for click element
+    	 * @param vars: {json} parameters required by apple metrics 
+    	 * @param loadEvent: {string} Typically 'load' or 'DOMContentLoaded' 
+    	 */
+	    click: function (selector, vars, loadEvent) {
+	        loadEvent = loadEvent || 'DOMContentLoaded';
+	        window.addEventListener(loadEvent, function () {
+				document.querySelector(selector).addEventListener('click', function (e) {
+					Omniture.fire(vars);
+				}, false);
+			}, false);
+	    }
+	};
+}();
--- js/passbridge.js
+++ js/passbridge.js
@@ -1,21 +1,27 @@
 /**
  * JavaScript bridge to Passbook webservice for mobile Safari on iOS6
+ * Self-executing module
  */
 var passbridge = function(){
 	var then,
 		divLoading = document.querySelector(".spinner"),
 		divLoaded = document.querySelector(".passbook");	
 	
-	// Prevent scrolling until loading is complete (required!)
-	var preventScrolling = function(e){
+	/**
+	 * Prevent scrolling until loading is complete (required!)
+	 */
+	function preventScrolling(e){
 		e.preventDefault();
 	};
 	
-	var loading = function(){
+	/**
+	 * Display the "loading" message before pass has been generated
+	 */
+	function loading(){
 		// Fade in 'loading' message
 		divLoading.style.opacity = 1;
-		var divLoaded = document.querySelector(".passbook");
-		divLoaded.style.display = "none";
+		document.querySelector(".passbook").style.display = "none";
+		
 		// Prevent false positive for overlay detection
 		window.addEventListener('touchmove', preventScrolling, true);
 		
@@ -25,7 +31,14 @@ var passbridge = function(){
 		}, 500);
 	};
 	
-	var loaded = function(){
+	/**
+	 * Display the "loaded" message after pass created
+	 */
+	function loaded(){
+		// Fire omniture event 'Pass Created.'
+		// This is set up in index.html (see omniture.js batch method)
+		Omniture.batch();
+		
 		// It is safe to allow scrolling again
 		window.removeEventListener('touchmove', preventScrolling, true);
 		
@@ -38,13 +51,16 @@ var passbridge = function(){
 		divLoading.style.opacity = 0;
 	};
 	
+	/**
+	 * Wrap the onload event, call loading method, and begin overlay detection polling
+	 */
 	return window.addEventListener('load', function() {	
 		// Show 'loading' msg	
 		loading();
 			
 		// Poll for >300ms pause in JS, assume Passbook overlay (page !visible)
 		window.webkitRequestAnimationFrame(function(now){
-			if( then !== undefined && now - then > 300) {
+			if( then !== undefined && now - then > 300 ) {
 				// Show 'loaded' msg	
 				loaded();
 			} else {
